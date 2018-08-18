2039c0bc9e9228c701281ee6d4fc09fe0f64e39b
Juan M.vi <earth001@gmail.com>
2018-08-18 14:25:36 -0300

    Estructura inicial class server

---
+++ class-server/src/main/java/uy/edu/ude/classserver/ClassServerApplication.java
@@ -0,0 +1,12 @@
+package uy.edu.ude.classserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassServerApplication.class, args);
+	}
+}
